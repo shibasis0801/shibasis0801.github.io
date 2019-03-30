c2321c50612a87406911506d6aa5f8181038fbbe
shibasis <shibasis0801@gmail.com>
2019-03-31 00:06:11 +0530

    Modularize this horrible mess of a codebase

--- scripts/todoapp.js
+++ scripts/todoapp.js
@@ -1,5 +1,29 @@
+/**
+ * Horrible Spaghetti Code right now.
+ * Have discovered modules
+ * 
+ * Create a button object with functions to create submit button , close button
+ * 
+ * Create similar for list item and input group
+ * 
+ * Then use this file to orchestrate creation.
+ * 
+ * :D :D
+ * 
+ * Loving JavaScript.
+ * 
+ * Will TypeScript be a good option? 
+ * Looks like Kotlin, lets see.
+ */
+
 const todoList = document.getElementById("list-todo");
 const buttonAddItem = document.getElementById("button-add-item");
+
+import { View } from './view.mjs'
+
+todoList.appendChild(
+    View.li("Hello Modular World")
+);
 
 let counter = 0;
 
@@ -41,10 +65,26 @@ function createListItem(message) {
     const textNode = document.createTextNode(message);
     listItem.appendChild(textNode);
 
+    const button = document.createElement('button');
+    button.textContent = "×";
+    button.className = 'close';
+
+    button.onclick = (event) => {
+        todoList.removeChild(button.parentNode)
+    } 
+
+    listItem.onclick = (event) => {
+        const inputElement = createInputElement(100, (event) => console.log(event), () => console.log("Cancelled"), "Hellow Baby");
+        todoList.removeChild(listItem);
+        todoList.appendChild(inputElement);
+    }
+
+    listItem.appendChild(button);
+
     return listItem;
 }
 
-function createInputElement(id, onSubmit, onCancel) {
+function createInputElement(id, onSubmit, onCancel, text) {
 
     const outerDiv = document.createElement('div');
     outerDiv.classList.add("input-group", "mb-3");
@@ -55,6 +95,8 @@ function createInputElement(id, onSubmit, onCancel) {
     editText.type = "text";
     editText.className = "form-control"
     editText.placeholder = "Enter Todo Item";
+    if ( text !== null )
+        editText.value = text;
 
     const innerDiv = document.createElement('div');
     innerDiv.className = "input-group-append";
@@ -78,3 +120,28 @@ function createInputElement(id, onSubmit, onCancel) {
 
     return outerDiv;
 }
+
+document.getElementById('button-test-api')
+    .addEventListener('click', event => {
+
+        const fakeMemberPromise = count => new Promise((resolves, rejects) => {
+
+            const api = `https://api.randomuser.me/?nat=IN&results=${count}`;
+            const request = new XMLHttpRequest();
+            request.open('GET', api);
+            request.onload = () => {
+                (request.status === 200) ? 
+                    resolves(JSON.parse(request.response).results) : 
+                    reject(Error(request.statusText));
+            };
+            request.onerror = (error) => rejects(error);
+            request.send();
+
+        });
+
+        fakeMemberPromise(5).then(
+                members => console.log(members),
+                error => console.error(error)
+        );
+
+    });
